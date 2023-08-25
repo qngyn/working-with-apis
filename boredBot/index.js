84f8c03ff985071aafbe7e0bab6d12142a9c6078
Quynh Nguyen <qtn2@case.edu>
2023-08-25 15:10:40 -0400

    finish the project

--- boredBot/index.js
+++ boredBot/index.js
@@ -16,3 +16,9 @@ Challenge:
 //         console.log(data)
 //         document.getElementById("activity-name").textContent = data.activity
 //     })
+
+document.getElementById("get-button").addEventListener('click', function(){
+  fetch("https://apis.scrimba.com/bored/api/activity").then(response => response.json()).then(data =>{
+    document.getElementById("container").textContent = `${data.activity}`
+})
+})
